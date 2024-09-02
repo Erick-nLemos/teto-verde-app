3644ef01befec81bcc95b5132c3be5604dcab573
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-02 16:47:17 -0300

    Segunda tela de cadastro

---
+++ app/src/main/java/com/ericklemos/tetoverde/Cadastro02.java
@@ -0,0 +1,14 @@
+package com.ericklemos.tetoverde;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Cadastro02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro2);
+    }
+}

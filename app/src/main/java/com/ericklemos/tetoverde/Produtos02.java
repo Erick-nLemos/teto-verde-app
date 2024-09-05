efc560f08ce0b3a3853ecb5c89fc7d7b45e99fb5
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-05 14:51:50 -0300

    Atualizacao Tela Produtos

---
+++ app/src/main/java/com/ericklemos/tetoverde/Produtos02.java
@@ -0,0 +1,14 @@
+package com.ericklemos.tetoverde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Produtos02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos02);
+    }
+}
---
+++ app/src/main/java/com/ericklemos/tetoverde/Produtos04.java
@@ -0,0 +1,14 @@
+package com.ericklemos.tetoverde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Produtos04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos04);
+    }
+}

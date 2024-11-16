be80622566907c126d92a39d7746bfdd21271eb4
Erick Lemos <ericklemos.dev@gmail.com>
2024-11-16 18:43:26 -0300

    classe para ignorar SSL concluida

--- app/src/main/java/com/ericklemos/tetoverde/configSSL/IgnoreSSL.java
+++ app/src/main/java/com/ericklemos/tetoverde/configSSL/IgnoreSSL.java
@@ -1,10 +1,33 @@
 package com.ericklemos.tetoverde.configSSL;
 
 import javax.net.ssl.*;
-import java.net.http.HttpClient;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 
 public class IgnoreSSL {
+    public static void ignorarCertificadosSSL() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        // Configura um contexto SSL para ignorar verificações de certificado.
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+        // Define o contexto SSL padrão.
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+    }
 }

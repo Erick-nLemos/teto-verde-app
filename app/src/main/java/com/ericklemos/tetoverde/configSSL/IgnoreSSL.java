9f9051f3aa0deb7b0b4f5e304fc6bfdca721bafe
Erick Lemos <ericklemos.dev@gmail.com>
2024-11-16 18:07:39 -0300

    configurando estrutura do projeto

---
+++ app/src/main/java/com/ericklemos/tetoverde/configSSL/IgnoreSSL.java
@@ -0,0 +1,10 @@
+package com.ericklemos.tetoverde.configSSL;
+
+import javax.net.ssl.*;
+import java.net.http.HttpClient;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+public class IgnoreSSL {
+}
